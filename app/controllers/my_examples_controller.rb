8ecb0cdbcd88f54fae44ac28dcdd45b1587ece57
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-11 14:37:37 -0400

    Creates the lotto path and method

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,7 +1,12 @@
 class MyExamplesController < ApplicationController
   def returns_fortune_method
-    fortunes = "You will have a good day!", "You will have a meh day.", "You will have a bad day."]
+    fortunes = ["You will have a good day!", "You will have a meh day.", "You will have a bad day."]
     num = rand(0..2)
-    render json: { message: "Here is your fortune!" fortunes[num] }
+    render json: { message: "Here is your fortune!", fortune: fortunes[num] }
+  end
+
+  def lotto_method
+    num = rand(1..60)
+    render json: { message: "This is the winning number", lotto: num }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/fortune_path", controller: "my_examples", action: "returns_fortune_method"
 end
+
+Rails.application.routes.draw do
+  get "/lotto", controller: "my_examples", action: "lotto_method"
+end
