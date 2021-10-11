4a894393a4e606c4edfa14f4b47afce84004b281
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-11 14:13:36 -0400

    Creates parts of the fortune method

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,5 +1,7 @@
 class MyExamplesController < ApplicationController
   def returns_fortune_method
-    render json: { message: "hello" }
+    fortunes = "You will have a good day!", "You will have a meh day.", "You will have a bad day."]
+    num = rand(0..2)
+    render json: { message: "Here is your fortune!" fortunes[num] }
   end
 end
